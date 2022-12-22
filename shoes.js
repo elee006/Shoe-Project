50340db5cb7658849fe328a758a05eb19aa861da
Erica Lee <elee006@odu.edu>
2022-12-22 17:19:34 -0600

    images are kinda working

--- shoes.js
+++ shoes.js
@@ -1,30 +1,40 @@
-import fetch from 'node-fetch';
-//const http = require("http");
-/*const hostname = "127.0.0.1";
-const port = 8000;
-const s = http.createServer(function(req, res){
-  res.writeHead(200, { "Content-Type": "text/plain" });
 
-  res.end("Hello World\n");
-});
-s.listen(port, hostname, function () {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});*/
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '56a6db7998msh79e284753227031p1b6cc5jsn3b5d1ced5855',
-		'X-RapidAPI-Host': 'sneaker-database-stockx.p.rapidapi.com'
-	}
-};
+var api_url = 'https://sneaker-database-stockx.p.rapidapi.com/mostpopular';
 
-fetch('https://sneaker-database-stockx.p.rapidapi.com/mostpopular', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-  app.use(function (err, req, res, next) {
-    console.error(err);
-    res.set('Content-Type', 'text/html');
-    res.status(500).send('<h1>Internal Server Error</h1>');
-  });
+  var options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '56a6db7998msh79e284753227031p1b6cc5jsn3b5d1ced5855',
+      'X-RapidAPI-Host': 'sneaker-database-stockx.p.rapidapi.com'
+    }
+  };
+
+  fetch(api_url, options)
+  .then((response) => {
+  if(response.ok){
+    return response.json();
+  }
+  else{
+    throw new Error("NETWORK RESPONSE ERROR");
+  }
+  })
+
+  .then((data) => {console.log(data)
+    loadPopImage(data)
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+
+
+
+function loadPopImage(d) {
+  var shoeimg = [];
+  for(let i = 0; i < 7; i++)
+  {
   
+  shoeimg[i] = d[i].thumbnail
+  
+ 
+  }
+  console.log(shoeimg);
+  document.getElementById("myImg").src = shoeimg;
+}
